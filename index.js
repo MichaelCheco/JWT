8a487f44914e48ab203ef05cdaa98cc1ce51bd9a
Michael <checomichael2@gmail.com>
2019-03-06 17:36:53 -0500

    jwt / login

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 8888;
+const users = [
+	{ id: 1, username: 'michael', password: 'password' },
+	{ id: 2, username: 'dave', password: 'password' },
+	{ id: 3, username: 'jessica', password: 'password' },
+];
 
 app.post('/login', (req, res) => {
-	const user = req.body.username;
+	const { username, password } = req.body;
+	if (!username || !password) {
+		res.status(400).send('Provide a username & Password');
+		return;
+	}
+	const user = users.find(
+		u => u.username === username && u.password === password
+	);
+	if (!user) {
+		res.status(401).send('User not found');
+		return;
+	}
+	const token = jwt.sign({ sub: user.id, username: user.username }, 'secret');
 
-	res.status(200).send(`You logged in with ${user}`);
+	res.status(200).send({ access_token: token });
 });
 
 app.get('/status', (req, res) => {
